e18dea859f27bfbbfefda5fab683fa0575f75cd3
Myuuiii <myu@subsilence.nl>
2021-07-29 21:31:11 +0200

    Yup

--- AzurLaneAPI/Controllers/ShipsControllerLvl120Stats.cs
+++ AzurLaneAPI/Controllers/ShipsControllerLvl120Stats.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using AzurLaneClasses;
 using AzurLaneClasses.Ship;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AzurLaneAPI.Controllers
 {
@@ -17,7 +18,9 @@ namespace AzurLaneAPI.Controllers
                 AzurLaneDbContext ctx = new AzurLaneDbContext();
                 if (ctx.Ships.Any(ship => ship.Id == id))
                 {
-                    return ctx.Ships.Single(ship => ship.Id == id).Level120Stats;
+                    return ctx.Ships
+                        .Include(s => s.Level120Stats)
+                        .Single(ship => ship.Id == id).Level120Stats;
                 }
                 else
                 {
@@ -40,7 +43,10 @@ namespace AzurLaneAPI.Controllers
                 AzurLaneDbContext ctx = new AzurLaneDbContext();
                 if (ctx.Ships.Any(ship => ship.Id == id))
                 {
-                    Ship selectedShip = ctx.Ships.Single(ship => ship.Id == id);
+                    Ship selectedShip = ctx.Ships
+                        .Include(s => s.Level120Stats)
+                        .Single(ship => ship.Id == id);
+
                     if (selectedShip.Level120Stats == null)
                     {
                         selectedShip.Level120Stats = stats;
@@ -86,7 +92,10 @@ namespace AzurLaneAPI.Controllers
                 AzurLaneDbContext ctx = new AzurLaneDbContext();
                 if (ctx.Ships.Any(ship => ship.Id == id))
                 {
-                    Ship selectedShip = ctx.Ships.Single(ship => ship.Id == id);
+                    Ship selectedShip = ctx.Ships
+                        .Include(s => s.Level120Stats)
+                        .Single(ship => ship.Id == id);
+                        
                     selectedShip.Level120Stats = null;
                     await ctx.SaveChangesAsync();
                     return Ok();
