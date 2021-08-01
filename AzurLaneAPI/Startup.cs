e5c75d86fedc1006e2b91c179ab60f0de32c6b4a
Myuuiii <myu@subsilence.nl>
2021-08-01 04:09:16 +0200

    Version Change

--- AzurLaneAPI/Startup.cs
+++ AzurLaneAPI/Startup.cs
@@ -38,7 +38,7 @@ namespace AzurLaneAPI
             services.AddSwaggerGen(c =>
             {
                 // c.DocumentFilter<RemoveSchemasDocumentFilter>();
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AzurLaneAPI", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AzurLaneAPI", Version = "v1.0.2" });
                 c.IncludeXmlComments(xmlPath);
             });
         }
