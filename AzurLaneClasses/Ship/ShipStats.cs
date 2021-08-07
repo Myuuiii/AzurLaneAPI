c703ed0097c6c45cadc5fb6dd868de3fc8cbe070
Myuuiii <myu@subsilence.nl>
2021-08-08 00:30:23 +0200

    Removed ShipStats Constructor

--- AzurLaneClasses/Ship/ShipStats.cs
+++ AzurLaneClasses/Ship/ShipStats.cs
@@ -11,27 +11,6 @@ namespace AzurLaneClasses.Ship
     public class ShipStats
     {
         public ShipStats() { }
-        public ShipStats(BaseStats stats)
-        {
-
-            this.Luck = String.IsNullOrEmpty(stats.luck) ? 0 : Convert.ToInt32(stats.luck);
-            this.Armor = stats.armor;
-            this.Speed = String.IsNullOrEmpty(stats.speed) ? 0 : Convert.ToInt32(stats.speed);
-            this.Health = String.IsNullOrEmpty(stats.health) ? 0 : Convert.ToInt32(stats.health);
-            this.Firepower = String.IsNullOrEmpty(stats.firepower) ? 0 : Convert.ToInt32(stats.firepower);
-            this.AntiAir = String.IsNullOrEmpty(stats.antiair) ? 0 : Convert.ToInt32(stats.antiair);
-            this.Torpedo = String.IsNullOrEmpty(stats.torpedo) ? 0 : Convert.ToInt32(stats.torpedo);
-            this.Evasion = String.IsNullOrEmpty(stats.evasion) ? 0 : Convert.ToInt32(stats.evasion);
-            this.Aviation = String.IsNullOrEmpty(stats.aviation) ? 0 : Convert.ToInt32(stats.aviation);
-            this.OilConsumption = String.IsNullOrEmpty(stats.oilConsumption) ? 0 : Convert.ToInt32(stats.oilConsumption);
-            this.Reload = String.IsNullOrEmpty(stats.reload) ? 0 : Convert.ToInt32(stats.reload);
-            this.AntiSubmarine = String.IsNullOrEmpty(stats.antisubmarineWarfare) ? 0 : Convert.ToInt32(stats.antisubmarineWarfare);
-            this.Oxygen = String.IsNullOrEmpty(stats.oxygen) ? 0 : Convert.ToInt32(stats.oxygen);
-            this.Ammunition = String.IsNullOrEmpty(stats.ammunition) ? 0 : Convert.ToInt32(stats.ammunition);
-            this.Accuracy = String.IsNullOrEmpty(stats.accuracy) ? 0 : Convert.ToInt32(stats.accuracy);
-            this.HuntingRange = stats.huntingRange;
-        }
-
 
         [JsonIgnore] public Guid Id { get; set; } = Guid.NewGuid();
         public Int32 Luck { get; set; }
