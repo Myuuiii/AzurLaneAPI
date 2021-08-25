c76b15c0027548e12ddc218b5f4453eb280fa7dd
Myuuiii <mute@mutedevs.nl>
2021-08-25 11:16:33 +0200

    Created Method

--- AzurLaneAPI/Startup.cs
+++ AzurLaneAPI/Startup.cs
@@ -23,7 +23,7 @@ namespace AzurLaneAPI
         {
             Configuration = configuration;
         }
-
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -41,7 +41,7 @@ namespace AzurLaneAPI
             services.AddSwaggerGen(c =>
             {
                 // c.DocumentFilter<RemoveSchemasDocumentFilter>();
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AzurLaneAPI", Version = "v1.2.22" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AzurLaneAPI", Version = "v1.2.25" });
                 c.IncludeXmlComments(xmlPath);
             });
         }
