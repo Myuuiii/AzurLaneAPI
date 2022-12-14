1e23badcb7638e78db30d4a7c7bd314a10c2b0ae
Myuuiii <mute@mutedevs.nl>
2022-12-14 17:46:26 +0100

    Class Scraper and Subclass Scraper Sketch Base

---
+++ AzurLaneAPI.Scraper/Scrapers/ClassScraper.cs
@@ -0,0 +1,8 @@
+﻿namespace AzurLaneAPI.Scraper.Scrapers;
+
+public class ClassScraper
+{
+	// Could fetch from this: https://azurlane.koumakan.jp/wiki/List_of_Ships_by_Type to get a list
+	// After which details can be scraped from urls like this https://azurlane.koumakan.jp/wiki/Category:Light_cruisers
+	// It also has a list of all subclasses
+}
---
+++ AzurLaneAPI.Scraper/Scrapers/SubclassScraper.cs
@@ -0,0 +1,8 @@
+﻿namespace AzurLaneAPI.Scraper.Scrapers;
+
+/// <summary>
+/// Please see <see cref="Scrapers.ClassScraper"/> for more information.
+/// </summary>
+public class SubclassScraper
+{
+}
